00305a0be305ba9f876c1608959aee3681957ed9
SoulofAO <sgsggssg1@gmail.com>
2022-01-12 20:15:09 +0300

    Clear_After_xperement

--- Stole_DaVInchy/Intermediate/Build/Win64/UE4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30137
-#include "E:/ASD/FlyProject/Stole_DaVInchy/Intermediate/Build/Win64/UE4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Stole_DaVInchy/Source/Stole_DaVInchy/Check_Path_Ubgrade.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Check_Path_Ubgrade.h"
-
