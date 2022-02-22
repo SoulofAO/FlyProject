4ffd4798f1e9fcc816cacf470cf8d03f1f913c40
SoulofAO <sgsggssg1@gmail.com>
2022-02-22 08:59:42 +0400

    ggg

--- Stole_DaVInchy/Plugins/Navigation3D/Intermediate/Build/Win64/UE4Editor/Inc/Navigation3D/NavigationVolume3D.generated.h
+++ Stole_DaVInchy/Plugins/Navigation3D/Intermediate/Build/Win64/UE4Editor/Inc/Navigation3D/NavigationVolume3D.generated.h
@@ -16,8 +16,8 @@ class UObject;
 #endif
 #define NAVIGATION3D_NavigationVolume3D_generated_h
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS \
  \
 	DECLARE_FUNCTION(execGetDivisionSize); \
 	DECLARE_FUNCTION(execGetDivisionsZ); \
@@ -29,7 +29,7 @@ class UObject;
 	DECLARE_FUNCTION(execFindPath);
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
  \
 	DECLARE_FUNCTION(execGetDivisionSize); \
 	DECLARE_FUNCTION(execGetDivisionsZ); \
@@ -41,7 +41,7 @@ class UObject;
 	DECLARE_FUNCTION(execFindPath);
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS_NO_PURE_DECLS \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesANavigationVolume3D(); \
 	friend struct Z_Construct_UClass_ANavigationVolume3D_Statics; \
@@ -50,7 +50,7 @@ public: \
 	DECLARE_SERIALIZER(ANavigationVolume3D)
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS \
 private: \
 	static void StaticRegisterNativesANavigationVolume3D(); \
 	friend struct Z_Construct_UClass_ANavigationVolume3D_Statics; \
@@ -59,7 +59,7 @@ public: \
 	DECLARE_SERIALIZER(ANavigationVolume3D)
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_STANDARD_CONSTRUCTORS \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API ANavigationVolume3D(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ANavigationVolume3D) \
@@ -72,7 +72,7 @@ private: \
 public:
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ENHANCED_CONSTRUCTORS \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API ANavigationVolume3D(ANavigationVolume3D&&); \
@@ -83,7 +83,7 @@ public: \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ANavigationVolume3D)
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_PRIVATE_PROPERTY_OFFSET \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_PRIVATE_PROPERTY_OFFSET \
 	FORCEINLINE static uint32 __PPO__DefaultSceneComponent() { return STRUCT_OFFSET(ANavigationVolume3D, DefaultSceneComponent); } \
 	FORCEINLINE static uint32 __PPO__ProceduralMesh() { return STRUCT_OFFSET(ANavigationVolume3D, ProceduralMesh); } \
 	FORCEINLINE static uint32 __PPO__DivisionsX() { return STRUCT_OFFSET(ANavigationVolume3D, DivisionsX); } \
@@ -95,27 +95,27 @@ public: \
 	FORCEINLINE static uint32 __PPO__Color() { return STRUCT_OFFSET(ANavigationVolume3D, Color); }
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_12_PROLOG
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_GENERATED_BODY_LEGACY \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_12_PROLOG
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_PRIVATE_PROPERTY_OFFSET \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_STANDARD_CONSTRUCTORS \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_PRIVATE_PROPERTY_OFFSET \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_GENERATED_BODY \
+#define Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_PRIVATE_PROPERTY_OFFSET \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS_NO_PURE_DECLS \
-	FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ENHANCED_CONSTRUCTORS \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_PRIVATE_PROPERTY_OFFSET \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS_NO_PURE_DECLS \
+	Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -123,7 +123,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 template<> NAVIGATION3D_API UClass* StaticClass<class ANavigationVolume3D>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FlyProject_2_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h
+#define CURRENT_FILE_ID Stole_DaVInchy_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
